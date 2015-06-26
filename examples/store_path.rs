8c74c890409f714fe0d173579e1575bdc55e2e20
Ivan Jager <aij+git@mrph.org>
2015-06-25 21:58:49 -0500

    Can now save directories

---
+++ examples/store_path.rs
@@ -0,0 +1,51 @@
+// Store a regular file into a storage pool and print the hash of the IndirectBlock
+
+extern crate rustc_serialize;
+extern crate docopt;
+extern crate capnp;
+extern crate cafs;
+
+use docopt::Docopt;
+use std::path::Path;
+use capnp::{MessageBuilder, MallocMessageBuilder};
+
+use cafs::storage_pool_leveldb::StoragePoolLeveldb;
+use cafs::cafs_capnp;
+
+// Write the Docopt usage string.
+static USAGE: &'static str = "
+Usage: store_path [-c] <path> <storage-pool>
+       store_path -c <storage-pool>
+
+Options:
+    -c, --create  Create new storage pool.
+";
+
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    arg_path: String,
+    arg_storage_pool: String,
+    flag_create: bool,
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|d| d.decode())
+                            .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
+
+    let stor = StoragePoolLeveldb::open(Path::new(&args.arg_storage_pool), args.flag_create).unwrap();
+    if (args.arg_path != "") {
+        let publisher = cafs::Publisher::new(stor);
+        let mut message = MallocMessageBuilder::new_default();
+        let mut fref = message.init_root::<cafs_capnp::reference::Builder>();
+        let res = publisher.save_path(Path::new(&args.arg_path), &mut fref);
+        match res {
+            Ok(fref) =>
+                println!("Success!"),
+            //println!("{:?}", fref),
+            Err(e) =>
+                println!("Error: {:?}", e),
+        }
+    }
+}
--- src/cafs_publish.rs
+++ src/cafs_publish.rs
@@ -1,7 +1,8 @@
 use std::path::Path;
 use std::io;
 use std::io::Read;
-use std::fs::{File, OpenOptions};
+use std::fs;
+use std::fs::{DirEntry, File, FileType, OpenOptions};
 use std::io::Error;
 
 use capnp::{MessageBuilder, MallocMessageBuilder};
@@ -22,13 +23,30 @@ impl Publisher {
     pub fn new(s: storage_pool_leveldb::StoragePoolLeveldb) -> Publisher {
         Publisher{ storage: s }
     }
-    fn save_block(&self, block:&[u8]) -> Result<Sha256, Error> {
+    fn save_raw_block(&self, block:&[u8]) -> Result<Sha256, Error> {
         match self.storage.put(block) {
             Ok(x) => Ok(x),
             Err(e) => Err(Error::new(io::ErrorKind::Other, e))
         }
     }
 
+    fn save_data<'a,'b>(&self, data:&[u8], dataref: &'b mut cafs_capnp::reference::data_ref::Builder<'a>) -> Result<&'b cafs_capnp::reference::data_ref::Builder<'a>, Error> {
+        // FIXME: Assumes data < BLOCK_SIZE
+        let hash = try!(self.save_raw_block(data));
+        {
+            let mut rawblock = dataref.borrow().init_indirect().init_rawblock();
+            rawblock.set_sha256(hash.as_slice());
+        };
+        Ok(dataref)
+    }
+    /*
+    fn save_block(&self, block:&[u8], res: &mut cafs_capnp::reference::block_ref::Builder) -> Result<&cafs_capnp::reference::block_ref::Builder, Error> {
+    }
+
+    fn save_data(&self, data: Reader, res: &mut cafs_capnp::reference::data_ref::Builder) -> Result<cafs_capnp::reference::data_ref::Builder, Error> {
+        
+    }
+     */
     pub fn export_file(&self, path:&Path) -> Result<Sha256, Error> {
         let mut f = try!(File::open(path));
         let mut buf = [0u8; BLOCK_SIZE];
@@ -42,7 +60,7 @@ impl Publisher {
             // BLOCK_SIZE bytes unless we are at the end of the file?
             let n = try!(f.read(&mut buf));
             if n == 0 { break; }
-            let hash = try!(self.save_block(&buf[0..n]));
+            let hash = try!(self.save_raw_block(&buf[0..n]));
             blocks.push((hash,n));
         }
 
@@ -64,6 +82,62 @@ impl Publisher {
         {
             serialize_packed::write_message(&mut encoded, &mut message);
         }
-        self.save_block(&encoded)
+        self.save_raw_block(&encoded)
+    }
+
+    pub fn save_file<'a,'b>(&self, path:&Path, refb: &'b mut cafs_capnp::reference::Builder<'a>) -> Result<&'b cafs_capnp::reference::Builder<'a>, Error> {
+        //let mut message = MallocMessageBuilder::new_default();
+        let hash = try!(self.export_file(path));
+        //let mut refb = message.init_root::<cafs_capnp::reference::Builder>();
+        {
+            let mut dataref = refb.borrow().init_file();
+            let mut rawblock = dataref.borrow().init_indirect().init_rawblock();
+            rawblock.set_sha256(hash.as_slice());
+
+        }
+        Ok(refb)
+    }
+
+    pub fn save_dir<'a, 'b>(&self, path:&Path, refb: &'b mut cafs_capnp::reference::Builder<'a>) -> Result<&'b cafs_capnp::reference::Builder<'a>, Error> {
+        
+        let readir: Vec<DirEntry> =
+            try!(fs::read_dir(path))
+            .flat_map(|e| e) // FIXME: Ignores error entries.
+            .collect();
+        
+        let mut message = MallocMessageBuilder::new_default();
+        let mut dirb = message.init_root::<cafs_capnp::directory::Builder>();
+        let mut files = dirb.init_files(readir.len() as u32);
+
+        for (i, dentry) in readir.iter().enumerate() {
+            let ft = try!(dentry.file_type());
+            let mut entry = files.borrow().get(i as u32);
+            entry.set_name(&dentry.file_name().to_string_lossy());
+            let mut fref = entry.init_ref();
+            self.save_path_with_type(&dentry.path(), ft, &mut fref);
+        }
+        {
+            let dref = refb.borrow().init_directory();
+            //dref.;
+        }
+        Ok(refb)
+    }
+
+    fn save_path_with_type<'a, 'b>(&self, path:&Path, typ: FileType, refb: &'b mut cafs_capnp::reference::Builder<'a>) -> Result<&'b cafs_capnp::reference::Builder<'a>, Error> {
+        if typ.is_dir() {
+            self.save_dir(path, refb)
+        } else if typ.is_file() {
+            self.save_file(path, refb)
+        } else if typ.is_symlink() {
+            panic!("Symlinks are not yet supported.")
+        } else {
+            panic!("Unexpected/unknown file type.")
+        }
+     
+    }
+
+    pub fn save_path<'a, 'b>(&self, path:&Path, refb: &'b mut cafs_capnp::reference::Builder<'a>) -> Result<&'b cafs_capnp::reference::Builder<'a>, Error> {
+        let md = try!(fs::symlink_metadata(path));
+        self.save_path_with_type(path, md.file_type(), refb)
     }
 }
